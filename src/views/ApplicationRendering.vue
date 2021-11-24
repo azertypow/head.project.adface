89b056422e458ad954a2d8f7e6020b978fb4da85
azertypow <azertypow@gmail.com>
2021-11-24 11:04:29 +0100

    feat: add restart button

--- src/views/ApplicationRendering.vue
+++ src/views/ApplicationRendering.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an pop up page render</h1>
+    <router-link
+        to="/"
+    >restart experience</router-link>
+
+    <button>share result</button>
+
   </div>
 </template>
