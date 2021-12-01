ff11fc8ccbd8079e4121cd7be1d0690dd06df3f5
azertypow <azertypow@gmail.com>
2021-12-01 20:13:23 +0100

    feat: init layout for rendering view

--- src/views/ApplicationRendering.vue
+++ src/views/ApplicationRendering.vue
@@ -4,7 +4,30 @@
     <main
         class="v-application-rendering__layout"
     >
-      You were targeted with these ads because you are a
+
+
+      <div
+          class="v-application-rendering__layout__img"
+      >
+        <img src="../assets/a.png" alt="">
+      </div>
+
+      <div
+          class="v-application-rendering__layout__id"
+      >ADface1638384486059
+      </div>
+
+      <div
+          class="v-application-rendering__layout__txt"
+      >You were targeted with these ads because you are a <span>latino</span> <span>women</span> of <span>35</span> years old.
+        <br>You are disgusted and look smart but really poor.
+      </div>
+
+      <div
+          class="v-application-rendering__layout__footer"
+      >addface.com
+      </div>
+
     </main>
 
 <!--    <div-->
@@ -73,8 +96,51 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .v-application-rendering {
-
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: white;
 }
 
+.v-application-rendering__layout {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  background: var(--site-color--background);
+  padding: var(--text-line-height);
+  width: 670px;
+}
+
+.v-application-rendering__layout__img {
+  width: 50%;
+  padding-right: var(--text-line-height);
+  box-sizing: border-box;
+
+  > img {
+    display: block;
+    width: 100%;
+  }
+}
+
+.v-application-rendering__layout__id {
+  width: 50%;
+}
+
+.v-application-rendering__layout__txt {
+  width: 100%;
+  padding-top: var(--text-line-height);
+  padding-bottom: var(--text-line-height);
+}
+
+.v-application-rendering__layout__footer {
+  padding-top: var(--text-line-height);
+  width: 66.66666%;
+}
 
 </style>
